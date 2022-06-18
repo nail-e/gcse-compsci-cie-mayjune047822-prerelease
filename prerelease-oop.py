8848c587b911af73c7b00427db085158385a84a8
nail-e <elianinigorieza@gmail.com>
2022-06-18 10:57:20 +0400

    Finished (20 lines shorter than prerelease.py!)

--- prerelease-oop.py
+++ prerelease-oop.py
@@ -4,9 +4,8 @@ from datetime import date, timedelta
 import random
 
 totalcost = 0
-discountcost = 0
-discountcountcounter = 0
-discountvaluecounter = 0
+ticketcount = 0
+discountcount = 0
 extracost = 0
 
 def breakline():
@@ -16,23 +15,19 @@ def line():
     print("------------------------")
 
 class Ticket:
-    def __init__(self, tickettype, onedaycost, twodaycost, discountcostoneday, discountcosttwoday, discountcount, discountweight):
+    def __init__(self, tickettype, onedaycost, twodaycost):
         self.tickettype = tickettype
         self.onedaycost = onedaycost
         self.twodaycost = twodaycost
-        self.discountcostoneday = discountcostoneday
-        self.discountcosttwoday = discountcosttwoday
-        self.discountcount = discountcount
-        self.discountweight = discountweight 
 
-adult = Ticket("One Adult", 20.00, 30.00, 15.00, 22.50, 1, 2)
-child = Ticket("One Child", 12.00, 18.00, 15.00, 22.50, 1, 1)
-senior = Ticket("One Senior", 16.00, 24.00, 15.00, 22.50, 1, 2)
-family = Ticket("Family Ticket (up to 2 adults or seniors and 3 children)", 60.00, 90.00, 60.00, 90.00, 0, 0)
-group = Ticket("Groups of 6 people or more, price per person", 15.00, 22.50, 15.00, 22.50, 0, 0)
-lion = Ticket("Lion Feeding", 2.50, 2.50, 0, 0, 0, 0)
-penguin = Ticket("Penguin Feeding", 2.00, 2.00, 0, 0, 0, 0)
-barbecue = Ticket("Evening Barbecue (Two-day ticket only)", 0, 5.00, 0, 0, 0, 0)
+adult = Ticket("One Adult", 20.00, 30.00,)
+child = Ticket("One Child", 12.00, 18.00)
+senior = Ticket("One Senior", 16.00, 24.00)
+family = Ticket("Family Ticket (up to 2 adults or seniors and 3 children)", 60.00, 90.00)
+group = Ticket("Groups of 6 people or more, price per person", 15.00, 22.50)
+lion = Ticket("Lion Feeding", 2.50, 2.50)
+penguin = Ticket("Penguin Feeding", 2.00, 2.00)
+barbecue = Ticket("Evening Barbecue (Two-day ticket only)", 0, 5.00)
 
 print('Dates Available')
 print("Option 1 - ", date.today() + timedelta(days=0))
@@ -67,7 +62,7 @@ while dayvisitloop == 0:
         print(family.tickettype, "- $", family.twodaycost)
         print(group.tickettype, "- $", group.twodaycost)
     else:
-        print("Invalid Input")
+        print("Error: Bad Integer")
         dayvisitloop += 0
 
 breakline()
@@ -83,8 +78,8 @@ elif days == 2:
     print(penguin.tickettype, "- $", penguin.twodaycost)
     print(barbecue.tickettype, "- $", barbecue.twodaycost)
 
-costloop = 0
-specialticket = 0
+costloop = 0                #loop to input ticket type amount
+specialticket = 0           #loop to check if y or n is input
 while costloop == 0:
     if days == 1:
         while specialticket == 0:
@@ -95,19 +90,13 @@ while costloop == 0:
                 specialticket += 1
                 adultcount = int(input("How many adults? "))
                 totalcost += adultcount * adult.onedaycost
-                discountcost += adultcount * adult.discountcostoneday
-                discountcountcounter += adult.discountcount
-                discountvaluecounter += adult.discountweight
+                ticketcount += adultcount
                 childcount = int(input("How many children? "))
                 totalcost += childcount * child.onedaycost
-                discountcost += childcount * child.discountcostoneday
-                discountcountcounter += child.discountcount
-                discountvaluecounter += child.discountweight
+                ticketcount += childcount
                 seniorcount = int(input("How many seniors? "))
                 totalcost += seniorcount * senior.onedaycost
-                discountcost += seniorcount * senior.discountcostoneday
-                discountcountcounter += senior.discountcount
-                discountvaluecounter += senior.discountweight
+                ticketcount += seniorcount
                 costloop += 1
             elif specialticketcheck == "y":
                 specialticketcheck = True
@@ -118,7 +107,7 @@ while costloop == 0:
                 totalcost += groupcount * group.onedaycost
                 costloop += 1
             else:
-                print("Invalid Input")
+                print("Error: Bad Input")
     elif days == 2:
          while specialticket == 0:
             breakline()
@@ -128,19 +117,13 @@ while costloop == 0:
                 specialticket += 1
                 adultcount = int(input("How many adults? "))
                 totalcost += adultcount * adult.twodaycost
-                discountcost += adultcount * adult.discountcosttwoday
-                discountcountcounter += adult.discountcount
-                discountvaluecounter += adult.discountweight
+                ticketcount += adultcount
                 childcount = int(input("How many children? "))
                 totalcost += childcount * child.twodaycost
-                discountcost += childcount * child.discountcosttwoday
-                discountcountcounter += child.discountcount
-                discountvaluecounter += child.discountweight
+                ticketcount += childcount
                 seniorcount = int(input("How many seniors? "))
                 totalcost += seniorcount * senior.twodaycost
-                discountcost += seniorcount * senior.discountcosttwoday
-                discountcountcounter += senior.discountcount
-                discountvaluecounter += senior.discountweight
+                ticketcount += seniorcount
                 costloop += 1
             elif specialticketcheck == "y":
                 specialticketcheck = True
@@ -151,12 +134,46 @@ while costloop == 0:
                 totalcost += groupcount * group.twodaycost
                 costloop += 1
             else:
-                print("Invalid Input")
+                print("Error: Bad Input")
+
+extraloop = 0           #loop to check if extra costs are added
+if days == 1:
+    while extraloop == 0:
+        breakline()
+        lioncount = int(input("How many tickets for lion feeding? "))
+        extracost += lioncount * lion.onedaycost
+        penguincount = int(input("How many tickets for penguin feeding? "))
+        extracost += penguincount * penguin.onedaycost
+        extraloop += 1
+elif days == 2:
+    while extraloop == 0:
+        lioncount = int(input("How many tickets for lion feeding? "))
+        extracost += lioncount * lion.twodaycost
+        penguincount = int(input("How many tickets for penguin feeding? "))
+        extracost += penguincount * penguin.twodaycost
+        bbqcount = int(input("How many tickets for the evening barbecue? "))
+        extracost += bbqcount * barbecue.twodaycost
+        extraloop += 1
+
+breakline()
+if days == 1:
+    if totalcost == 76.00 or totalcost == 72.00 or totalcost == 68.00 and ticketcount == 6:
+        print("You are eligible for a family ticket which is cheaper than your current selection.")
+        discountcount += 60
+    elif ticketcount >= 6:
+        print("You have more than 6 people in your group and eligible for a group ticket for six people.")
+        discountcount += ticketcount * group.onedaycost
+elif days == 2:
+    if totalcost == 114.00 or totalcost == 108.00 or totalcost == 78.00 and ticketcount == 6:
+        print("You are eligible for a family ticket which is cheaper than your current selection.")
+        discountcount += 90
+    elif ticketcount >= 6:
+        print("You have more than 6 people in your group and eligible for a group ticket for six people.")
+        discountcount += ticketcount * group.twodaycost 
 
 #Final Output
 breakline()
 print('Your ticket is valid on', date.today() + timedelta(days=selecteddate-1), "for", days ,"day(s)")
-print("Your total, without discount, is ", totalcost)
-print("Your extra attraction total, without discount, is ", extracost)
-print("Your total, with discount is ", discountcost + extracost)
-print("Your booking number is,", random.randrange(1, 100000, 6))
+print("Your total is $", totalcost + extracost)
+print("Your total, with the best value is $", extracost + discountcount)
+print("Your booking number is", random.randrange(1, 100000, 6))
